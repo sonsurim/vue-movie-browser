cde1aa153efd3807f10421b3e34453037e4ebea0
sonsurim <surim014@naver.com>
2021-10-09 07:00:27 +0900

    style: 404 page layout

--- soncha/src/views/NotFound.vue
+++ soncha/src/views/NotFound.vue
@@ -1,8 +1,7 @@
 <template>
   <HomeHeader />
   <main class="notfound__main">
-    <h1>404</h1>
-    <h1>NOT FOUND</h1>
+    <h1>No Result!</h1>
   </main>
 </template>
 
@@ -17,6 +16,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.notfound__main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 200px);
+}
 h1 {
   font-size: 80px;
   font-weight: bold;
